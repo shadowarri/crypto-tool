6a26dc759bf776db15bf4768a799775a82f39856
Anatoly Petrov <shadowarri01@gmail.com>
2021-10-10 19:09:20 +0500

    broked program into modules and added CBC and CTR

--- crypto-tool/main.cc
+++ crypto-tool/main.cc
@@ -9,255 +9,83 @@
 #include <fstream>
 #include <cstring>
 #include <algorithm>
+#include <iomanip>
 #include <random> // для std::random_device и std::mt19937
 
 #include "CryptoContainer.h"
 
 const char * TEST_FILE_NAME = "test.txt";
 const char * TEST_CONTAINER_NAME = "test-container.si";
-const char * TEST_CONTAINER_NAME_WITH_CRYPT = "test-container-crypt.si";
 const char * TEST_KEY_CONTAINER_NAME = "KeyContainer.si";
 constexpr uint32_t BLOCK_SIZE = 32;//БИТЫ
 
-constexpr int t111[16] = {3, 5, 4, 8, 9, 1, 11, 13, 12, 0, 15, 2, 7, 6, 10, 14};
+constexpr int t111[16] = {	3, 5, 4, 8, 9, 1, 11, 13, 12, 0, 15, 2, 7, 6, 10, 14 };
+const char * CRYPT_NAME[4] = {"", "ECB_", "CBC_", "CTR_"};
+const char * IV = "abcd";
 
-void create_container()
+enum crypt_types
 {
-	using namespace crypto::container;
-	std::ifstream src_file;
+	RAW_CRYPT = 0,
+	ECB_CRYPT,
+	CBC_CRYPT,
+	CTR_CRYPT
+};
 
-	src_file.open(TEST_FILE_NAME, std::ios::binary | std::ios::ate);
-	size_t filesize = src_file.tellg();
-	src_file.seekg(0);
+int crypt_type = 2;
 
-	std::ofstream dst_file;
-	dst_file.open(TEST_CONTAINER_NAME, std::ios::binary);
-
-	header hdr {};
-	hdr.magic_word = MAGIC_WORD;
-	hdr.header_size = HEADER_SIZE;
-	hdr.payload = RAW;
-
-	dst_file.write(reinterpret_cast<char*>(&hdr), HEADER_SIZE);
-	metadata_file mdf {};
-	uint32_t name_length = strlen(TEST_FILE_NAME);
-	mdf.length = FILE_METADATA_SIZE + name_length + 1;
-	mdf.original_length = filesize;
-	mdf.block_size = BLOCK_SIZE;
-	mdf.block_count = filesize / (BLOCK_SIZE / 8);
-	if (filesize % (BLOCK_SIZE / 8) > 0)
+int get_int(const char *question) {
+	int result;
+	std::string inp;
+	re: std::cout << question;
+	std::getline(std::cin, inp);
 	{
-		mdf.block_count++;
-	}
-	dst_file.write(reinterpret_cast<char*>(&mdf), FILE_METADATA_SIZE);
-	dst_file.write(TEST_FILE_NAME, name_length + 1);
-
-	for (	uint64_t block = 0;
-			block < mdf.block_count;
-			++block) {
-		uint8_t buffer[BLOCK_SIZE / 8] {};
-		src_file.read(reinterpret_cast<char*>(&buffer[0]),
-				BLOCK_SIZE /8);
-		dst_file.write(reinterpret_cast<char*>(&buffer[0]),
-						BLOCK_SIZE /8);
-	}
-
-	src_file.close();
-	dst_file.close();
-}
-
-void create_container_with_crypt()
-{
-	using namespace crypto::container;
-	std::ifstream src_file;
-
-	src_file.open(TEST_FILE_NAME, std::ios::binary | std::ios::ate);
-	size_t filesize = src_file.tellg();
-	src_file.seekg(0);
-
-	std::ofstream dst_file;
-	dst_file.open(TEST_CONTAINER_NAME_WITH_CRYPT, std::ios::binary);
-
-	header hdr {};
-	hdr.magic_word = MAGIC_WORD;
-	hdr.header_size = HEADER_SIZE;
-	hdr.payload = KEY_DATA;
-
-	dst_file.write(reinterpret_cast<char*>(&hdr), HEADER_SIZE);
-	metadata_file mdf {};
-	uint32_t name_length = strlen(TEST_FILE_NAME);
-	mdf.length = FILE_METADATA_SIZE + name_length + 1;
-	mdf.original_length = filesize;
-	mdf.block_size = BLOCK_SIZE;
-	mdf.block_count = filesize / (BLOCK_SIZE / 8);
-	if (filesize % (BLOCK_SIZE / 8) > 0)
-	{
-		mdf.block_count++;
-	}
-	dst_file.write(reinterpret_cast<char*>(&mdf), FILE_METADATA_SIZE);
-	dst_file.write(TEST_FILE_NAME, name_length + 1);
-
-	std::ifstream key_file;
-	key_file.open(TEST_KEY_CONTAINER_NAME, std::ios::binary);
-	header key_hdr {};
-	key_file.read(reinterpret_cast<char*>(&key_hdr),
-			sizeof(header));
-
-	if (key_hdr.magic_word != MAGIC_WORD) {
-		std::cerr << "KEY FILE IS WRONG" << std::endl;
-		return;
-	}
-
-	if (key_hdr.payload != RAW)
-	{
-		std::cerr <<
-				"WRONG DATA IN KEY FILE" << std::endl;
-		return;
-	}
-	key_file.seekg(key_hdr.header_size);
-
-	uint64_t pos_after_key_header = key_file.tellg();
-
-	metadata_file mdk {};
-	key_file.read(reinterpret_cast<char*>(&mdk),
-			KEY_METADATA_SIZE);
-	key_file.seekg(pos_after_key_header + mdk.length);
-	uint8_t buffer_key[16] {};
-	key_file.read(reinterpret_cast<char*>(&buffer_key[0]),
-			16);
-	for (	uint64_t block = 0;
-			block < mdf.block_count;
-			++block) {
-		uint8_t buffer[BLOCK_SIZE / 8] {};
-		src_file.readsome(reinterpret_cast<char*>(&buffer[0]),
-				BLOCK_SIZE / 8);
-		uint8_t *forMerge = new uint8_t[2];
-		forMerge[0] = buffer[0];
-		forMerge[1] = buffer[1];
-		uint16_t Li =  *((uint16_t*)forMerge);
-
-		forMerge = new uint8_t[2];
-		forMerge[0] = buffer[2];
-		forMerge[1] = buffer[3];
-		uint16_t Ri = *((uint16_t*)forMerge);
-
-		for (unsigned int lap = 0; lap < 8; lap++)
+		std::stringstream inps { inp };
+		inps >> result;
 		{
-			uint16_t x = Li;
-			uint16_t F = (t111[(x >> 12) & 0x0F] << 12) +  (t111[(x >> 8) & 0x0F] << 8) + (t111[(x >> 4) & 0x0F] << 4) + t111[x & 0x0F];
-			forMerge = new uint8_t[2];
-			forMerge[0] = buffer_key[lap * 2];
-			forMerge[1] = buffer_key[lap * 2 + 1];
-			uint16_t key = *((uint16_t*)forMerge);
-			F ^= key;
-			F = (F << 3) | (F >> (16-3));
-			uint16_t oldLi = Li;
-			Li = Ri ^ F;
-			Ri = oldLi;
+			if (inps.fail()) {
+				if (std::cin.eof()) {
+					std::cout << "Ошибка ввода/вывода" << std::endl;
+					exit(1);
+				}
+				std::cout << "Некорректный ввод" << std::endl;
+				goto re;
+			} else if (not inps.eof()) {
+				std::cout << "Некорректный ввод" << std::endl;
+				goto re;
+			}
 		}
-		dst_file.write(reinterpret_cast<char*>(&Ri),
-						BLOCK_SIZE /16);
-		dst_file.write(reinterpret_cast<char*>(&Li),
-								BLOCK_SIZE /16);
 	}
 
-	src_file.close();
-	dst_file.close();
+	return result;
 }
 
-void extract_container()
-{
-	using namespace crypto::container;
-
-	std::ifstream src_file;
-
-	src_file.open(TEST_CONTAINER_NAME, std::ios::binary);
-	header hdr {};
-	src_file.read(reinterpret_cast<char*>(&hdr),
-			sizeof(header));
-
-	if (hdr.magic_word != MAGIC_WORD) {
-		std::cerr << "FILE IS WRONG" << std::endl;
-		return;
+int getAndCheckInRange(const char *question, int min, int max) {
+	for (;;) {
+		int returnInt = get_int(question);
+		if (returnInt >= min && returnInt <= max) {
+			return returnInt;
+		}
+		std::cout << "Введите подходящее число!" << std::endl;
+		continue;
 	}
-
-	if (hdr.payload != RAW)
-	{
-		std::cerr <<
-				"WRONG DATA IN FILE" << std::endl;
-		return;
-	}
-	src_file.seekg(hdr.header_size);
-
-	uint64_t pos_after_header = src_file.tellg();
-
-	metadata_file mdf {};
-	src_file.read(reinterpret_cast<char*>(&mdf),
-			FILE_METADATA_SIZE);
-	std::string original_filename = "Extract_";
-	char c;
-	while ((c = src_file.get()))
-	{
-		original_filename += c;
-	}
-
-	std::ofstream dst_file;
-	dst_file.open(original_filename.c_str(), std::ios::binary);
-	src_file.seekg(pos_after_header + mdf.length);
-
-	while (mdf.original_length > 0)
-	{
-		uint8_t buffer[BLOCK_SIZE / 8] {};
-		src_file.read(reinterpret_cast<char*>(&buffer[0]),
-				BLOCK_SIZE / 8);
-		uint64_t bytes_to_write = std::min<unsigned long>(4UL, mdf.original_length);
-		dst_file.write(reinterpret_cast<char*>(&buffer[0]),
-				bytes_to_write);
-		mdf.original_length -= bytes_to_write;
-	}
-
-	src_file.close();
-	dst_file.close();
-
+	return 0;
 }
 
-void extract_container_with_crypt()
+int setCryptType() {
+	std::cout << "Выберите режим шифрования(Сейчас выбран под п. " << (crypt_type + 1) << ") \n"
+				"1. Без шифрования \n"
+				"2. Сеть Фейстеля(ECB) \n"
+				"3. Сеть Фейстеля(CBC) \n"
+				"4. Сеть Фейстеля(CTR) \n"
+			<< std::endl;
+	crypt_type = getAndCheckInRange("Выберите пункт меню", 1, 4) - 1;
+
+	return 0;
+}
+
+void cryptFeistelNetwork(uint16_t &Li, uint16_t &Ri, bool uncrypt)
 {
 	using namespace crypto::container;
-
-	std::ifstream src_file;
-
-	src_file.open(TEST_CONTAINER_NAME_WITH_CRYPT, std::ios::binary);
-	header hdr {};
-	src_file.read(reinterpret_cast<char*>(&hdr),
-			sizeof(header));
-
-	if (hdr.magic_word != MAGIC_WORD) {
-		std::cerr << "FILE IS WRONG" << std::endl;
-		return;
-	}
-
-	if (hdr.payload != KEY_DATA)
-	{
-		std::cerr <<
-				"WRONG DATA IN FILE" << std::endl;
-		return;
-	}
-	src_file.seekg(hdr.header_size);
-
-	uint64_t pos_after_header = src_file.tellg();
-
-	metadata_file mdf {};
-	src_file.read(reinterpret_cast<char*>(&mdf),
-			FILE_METADATA_SIZE);
-	std::string original_filename = "Extract_with_encrypt_";
-	char c;
-	while ((c = src_file.get()))
-	{
-		original_filename += c;
-	}
-	src_file.seekg(pos_after_header + mdf.length);
-
 	std::ifstream key_file;
 	key_file.open(TEST_KEY_CONTAINER_NAME, std::ios::binary);
 	header key_hdr {};
@@ -287,48 +115,38 @@ void extract_container_with_crypt()
 	key_file.read(reinterpret_cast<char*>(&buffer_key[0]),
 			16);
 
-	std::ofstream dst_file;
-	dst_file.open(original_filename.c_str(), std::ios::binary);
-	while (mdf.original_length > 0)
+	for (unsigned int lap = 0; lap < 8; lap++)
 	{
-		uint8_t buffer[BLOCK_SIZE / 8] {};
-		src_file.read(reinterpret_cast<char*>(&buffer[0]),
-				BLOCK_SIZE / 8);
-		uint8_t *forMerge = new uint8_t[2];
-		forMerge[0] = buffer[0];
-		forMerge[1] = buffer[1];
-		uint16_t Li =  *((uint16_t*)forMerge);
-		forMerge = new uint8_t[2];
-		forMerge[0] = buffer[2];
-		forMerge[1] = buffer[3];
-		uint16_t Ri = *((uint16_t*)forMerge);
-		for (unsigned int lap = 0; lap < 8; lap++)
+		uint16_t x = Li;
+		uint16_t F = (t111[(x >> 12) & 0x0F] << 12) +  (t111[(x >> 8) & 0x0F] << 8) + (t111[(x >> 4) & 0x0F] << 4) + t111[x & 0x0F];
+		int positionKey0 = lap * 2;
+		int positionKey1 = lap * 2 + 1;
+		if (uncrypt)
 		{
-			uint16_t x = Li;
-			uint16_t F = (t111[(x >> 12) & 0x0F] << 12) +  (t111[(x >> 8) & 0x0F] << 8) + (t111[(x >> 4) & 0x0F] << 4) + t111[x & 0x0F];
-			forMerge = new uint8_t[2];
-			forMerge[0] = buffer_key[15 - (lap * 2 + 1)];
-			forMerge[1] = buffer_key[15 - (lap * 2)];
-			uint16_t key = *((uint16_t*)forMerge);
-			F ^= key;
-			F = (F << 3) | (F >> (16-3));
-			uint16_t oldLi = Li;
-			Li = Ri ^ F;
-			Ri = oldLi;
+			positionKey0 = 15 - (lap * 2 + 1);
+			positionKey1 = 15 - (lap * 2);
 		}
-		uint16_t *forMergePart = new uint16_t[2];
-		forMergePart[0] = Ri;
-		forMergePart[1] = Li;
-		uint32_t encryptPart = *((uint32_t*)forMergePart);
-		uint64_t bytes_to_write = std::min<unsigned long>(4UL, mdf.original_length);
-		dst_file.write(reinterpret_cast<char*>(&encryptPart),
-				bytes_to_write);
-		mdf.original_length -= bytes_to_write;
+		uint16_t key = (buffer_key[positionKey1] << 8) + buffer_key[positionKey0];
+		F ^= key;
+		F = (F << 3) | (F >> (16-3));
+		uint16_t oldLi = Li;
+		Li = Ri ^ F;
+		Ri = oldLi;
 	}
+	std::swap (Li, Ri);
+}
 
-	src_file.close();
-	dst_file.close();
-
+void increment_block(uint8_t * block, size_t sz)
+{
+	unsigned current_byte = 0;
+	while(current_byte < sz)
+	{
+		uint8_t old_value = block[current_byte];
+		uint8_t new_value = old_value + 1;
+		block[current_byte] = new_value;
+		if (new_value > old_value) return;
+		++current_byte;
+	}
 }
 
 void generate_key(uint32_t size_key)
@@ -370,13 +188,265 @@ void generate_key(uint32_t size_key)
 	dst_file.close();
 }
 
+void create_container()
+{
+	std::cout << "Convert" << std::endl;
+	using namespace crypto::container;
+	std::ifstream src_file;
+
+	src_file.open(TEST_FILE_NAME, std::ios::binary | std::ios::ate);
+	size_t filesize = src_file.tellg();
+	src_file.seekg(0);
+
+	std::ofstream dst_file;
+
+	dst_file.open(std::string(CRYPT_NAME[crypt_type]) + TEST_CONTAINER_NAME, std::ios::binary);
+
+	header hdr {};
+	hdr.magic_word = MAGIC_WORD;
+	hdr.header_size = HEADER_SIZE;
+	hdr.payload = crypt_type;
+
+	dst_file.write(reinterpret_cast<char*>(&hdr), HEADER_SIZE);
+
+	metadata_file mdf {};
+	uint32_t name_length = strlen(TEST_FILE_NAME);
+	mdf.length = FILE_METADATA_SIZE + name_length + 1;
+	mdf.original_length = filesize;
+	mdf.block_size = BLOCK_SIZE;
+	mdf.block_count = filesize / (BLOCK_SIZE / 8);
+	if (filesize % (BLOCK_SIZE / 8) > 0)
+	{
+		mdf.block_count++;
+	}
+	dst_file.write(reinterpret_cast<char*>(&mdf), FILE_METADATA_SIZE);
+	dst_file.write(TEST_FILE_NAME, name_length + 1);
+
+	uint32_t crypto_word = *((uint32_t*)IV);
+	for (	uint64_t block = 0;
+			block < mdf.block_count;
+			++block) {
+		uint8_t buffer[BLOCK_SIZE / 8] {};
+		src_file.readsome(reinterpret_cast<char*>(&buffer[0]),
+				BLOCK_SIZE / 8);
+
+		switch(crypt_type)
+		{
+		case ECB_CRYPT:{
+			uint16_t Li = (buffer[3] << 8) + buffer[2];
+			uint16_t Ri = (buffer[1] << 8) + buffer[0];
+			cryptFeistelNetwork(Li, Ri, false);
+			uint32_t cryptedParts = (Li << 16) + Ri;
+			dst_file.write(reinterpret_cast<char*>(&cryptedParts),
+									BLOCK_SIZE / 8);
+			break;
+		}
+		case CBC_CRYPT:{
+			uint32_t word = (buffer[3] << 24) + (buffer[2] << 16) + (buffer[1] << 8) + buffer[0];
+			word = word ^ crypto_word;
+			uint16_t Li = word >> 16 & 0xFFFF;
+			uint16_t Ri = word & 0xFFFF;
+			cryptFeistelNetwork(Li, Ri, false);
+			crypto_word = (Li << 16) + Ri;
+			dst_file.write(reinterpret_cast<char*>(&crypto_word),
+									BLOCK_SIZE /8);
+			break;
+		}
+		case CTR_CRYPT:{
+			uint16_t Li = crypto_word >> 16 & 0xFFFF;
+			uint16_t Ri = crypto_word & 0xFFFF;
+			cryptFeistelNetwork(Li, Ri, false);
+			uint32_t result_crypto_word = (Li << 16) + Ri;
+			uint32_t word = (buffer[3] << 24) + (buffer[2] << 16) + (buffer[1] << 8) + buffer[0];
+			uint32_t result = word ^ result_crypto_word;
+			dst_file.write(reinterpret_cast<char*>(&result),
+												BLOCK_SIZE /8);
+			uint8_t * crypto_word_to_blocks = new uint8_t[4];
+			crypto_word_to_blocks[3] = crypto_word&0xFF;
+			crypto_word_to_blocks[2] = (crypto_word >> 8)&0xFF;
+			crypto_word_to_blocks[1] = (crypto_word >> 16)&0xFF;
+			crypto_word_to_blocks[0] = (crypto_word >> 24)&0xFF;
+			increment_block(&crypto_word_to_blocks[0], BLOCK_SIZE);
+			crypto_word = (crypto_word_to_blocks[3] << 24) + (crypto_word_to_blocks[2] << 16) + (crypto_word_to_blocks[1] << 8) + crypto_word_to_blocks[0];
+			break;
+		}
+		case RAW_CRYPT:
+		default:{
+			dst_file.write(reinterpret_cast<char*>(&buffer[0]),
+									BLOCK_SIZE /8);
+			break;
+		}
+		}
+
+	}
+
+	src_file.close();
+	dst_file.close();
+}
+
+void extract_container()
+{
+	std::cout << "Extract" << std::endl;
+	using namespace crypto::container;
+
+	std::ifstream src_file;
+
+	src_file.open(std::string(CRYPT_NAME[crypt_type]) + TEST_CONTAINER_NAME, std::ios::binary);
+	header hdr {};
+	src_file.read(reinterpret_cast<char*>(&hdr),
+			sizeof(header));
+
+	if (hdr.magic_word != MAGIC_WORD) {
+		std::cerr << "FILE IS WRONG" << std::endl;
+		return;
+	}
+
+	if (hdr.payload != crypt_type)
+	{
+		std::cerr <<
+				"WRONG DATA IN FILE" << std::endl;
+		return;
+	}
+	src_file.seekg(hdr.header_size);
+
+	uint64_t pos_after_header = src_file.tellg();
+
+	metadata_file mdf {};
+	src_file.read(reinterpret_cast<char*>(&mdf),
+			FILE_METADATA_SIZE);
+	std::string original_filename = "Extract_with_encrypt_" + std::string(CRYPT_NAME[crypt_type]);
+	char c;
+	while ((c = src_file.get()))
+	{
+		original_filename += c;
+	}
+	src_file.seekg(pos_after_header + mdf.length);
+
+	std::ifstream key_file;
+	key_file.open(TEST_KEY_CONTAINER_NAME, std::ios::binary);
+	header key_hdr {};
+	key_file.read(reinterpret_cast<char*>(&key_hdr),
+			sizeof(header));
+
+	if (key_hdr.magic_word != MAGIC_WORD) {
+		std::cerr << "KEY FILE IS WRONG" << std::endl;
+		return;
+	}
+
+	if (key_hdr.payload != RAW)
+	{
+		std::cerr <<
+				"WRONG DATA IN KEY FILE" << std::endl;
+		return;
+	}
+	key_file.seekg(key_hdr.header_size);
+
+	uint64_t pos_after_key_header = key_file.tellg();
+
+	metadata_file mdk {};
+	key_file.read(reinterpret_cast<char*>(&mdk),
+			KEY_METADATA_SIZE);
+	key_file.seekg(pos_after_key_header + mdk.length);
+	uint8_t buffer_key[16] {};
+	key_file.read(reinterpret_cast<char*>(&buffer_key[0]),
+			16);
+
+	std::ofstream dst_file;
+	dst_file.open(original_filename.c_str(), std::ios::binary);
+	uint32_t crypto_word = *((uint32_t*)IV);
+	while (mdf.original_length > 0)
+	{
+		uint8_t buffer[BLOCK_SIZE / 8] {};
+		src_file.read(reinterpret_cast<char*>(&buffer[0]),
+				BLOCK_SIZE / 8);
+
+		uint64_t bytes_to_write = std::min<unsigned long>(4UL, mdf.original_length);
+
+		switch(crypt_type)
+		{
+		case ECB_CRYPT:{
+			uint16_t Li = (buffer[3] << 8) + buffer[2];
+			uint16_t Ri = (buffer[1] << 8) + buffer[0];
+			cryptFeistelNetwork(Li, Ri, true);
+			uint32_t encryptPart = (Li << 16) + Ri;
+			dst_file.write(reinterpret_cast<char*>(&encryptPart),
+							bytes_to_write);
+			break;
+		}
+		case CBC_CRYPT:{
+			uint16_t Li = (buffer[3] << 8) + buffer[2];
+			uint16_t Ri = (buffer[1] << 8) + buffer[0];
+			uint32_t crypto_word_save = (Li << 16) + Ri;
+			cryptFeistelNetwork(Li, Ri, true);
+			uint32_t word = (Li << 16) + Ri;
+			word = word ^ crypto_word;
+			crypto_word = crypto_word_save;
+			dst_file.write(reinterpret_cast<char*>(&word),
+							bytes_to_write);
+			break;
+		}
+		case CTR_CRYPT:{
+			uint16_t Li = crypto_word >> 16 & 0xFFFF;
+			uint16_t Ri = crypto_word & 0xFFFF;
+			cryptFeistelNetwork(Li, Ri, false);
+			uint32_t result_crypto_word = (Li << 16) + Ri;
+			uint32_t word = (buffer[3] << 24) + (buffer[2] << 16) + (buffer[1] << 8) + buffer[0];
+			uint32_t result = word ^ result_crypto_word;
+			dst_file.write(reinterpret_cast<char*>(&result),
+												BLOCK_SIZE /8);
+			uint8_t * crypto_word_to_blocks = new uint8_t[4];
+			crypto_word_to_blocks[3] = crypto_word&0xFF;
+			crypto_word_to_blocks[2] = (crypto_word >> 8)&0xFF;
+			crypto_word_to_blocks[1] = (crypto_word >> 16)&0xFF;
+			crypto_word_to_blocks[0] = (crypto_word >> 24)&0xFF;
+			increment_block(&crypto_word_to_blocks[0], BLOCK_SIZE);
+			crypto_word = (crypto_word_to_blocks[3] << 24) + (crypto_word_to_blocks[2] << 16) + (crypto_word_to_blocks[1] << 8) + crypto_word_to_blocks[0];
+			break;
+		}
+			break;
+		case RAW_CRYPT:
+		default:{
+			dst_file.write(reinterpret_cast<char*>(&buffer[0]),
+							bytes_to_write);
+			break;
+		}
+		}
+
+		mdf.original_length -= bytes_to_write;
+	}
+
+	src_file.close();
+	dst_file.close();
+
+}
+
 int main(int argc, char ** argv)
 {
-//	generate_key(16);
-//	create_container_with_crypt();
-	extract_container_with_crypt();
-//	create_container();
-//	extract_container();
-
+	for (;;) {
+		std::cout << "Меню \n"
+				"1. Зашрифровать  \n"
+				"2. Расшифровать \n"
+				"3. Создать ключ \n"
+				"4. Метод шифрования \n"
+				"5. Выход" << std::endl;
+		int intForSwitch = getAndCheckInRange("Выберите пункт меню", 1, 5);
+		switch (intForSwitch) {
+		case 1:
+			create_container();
+			break;
+		case 2:
+			extract_container();
+			break;
+		case 3:
+			generate_key(16);
+			break;
+		case 4:
+			setCryptType();
+			break;
+		case 5:
+			exit(0);
+			break;
+		}
+	}
 	return 0;
 }
